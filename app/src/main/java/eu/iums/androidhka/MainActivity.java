eb454bd85d60baa38c2c105e64dda82528d8f0c0
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2021-10-13 15:25:10 +0200

    Praxis-Vorlesung Teil I

---
+++ app/src/main/java/eu/iums/androidhka/MainActivity.java
@@ -0,0 +1,33 @@
+package eu.iums.androidhka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = this.findViewById(R.id.editText);
+        Button buttonActivity = this.findViewById(R.id.buttonActivity);
+
+        buttonActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = editText.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("TEXT", inputText);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
