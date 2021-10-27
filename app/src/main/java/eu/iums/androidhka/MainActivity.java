964540b242bb493742270a9d163275b19f025cf1
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2021-10-27 15:25:54 +0200

    Übung II

--- app/src/main/java/eu/iums/androidhka/MainActivity.java
+++ app/src/main/java/eu/iums/androidhka/MainActivity.java
@@ -1,12 +1,14 @@
 package eu.iums.androidhka;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        EditText editText = this.findViewById(R.id.editText);
-        Button buttonActivity = this.findViewById(R.id.buttonActivity);
-
-        buttonActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String inputText = editText.getText().toString();
-
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("TEXT", inputText);
-
-                startActivity(intent);
-            }
-        });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
