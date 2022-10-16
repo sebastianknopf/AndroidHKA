483535d6d7bbd72d3bf2b53c8387ef42fe4c6aa9
sebastianknopf <mail@sebastian-knopf.de>
2022-10-16 08:39:18 +0200

    code update for ex II

--- app/src/main/java/eu/iums/androidhka/MainActivity.java
+++ app/src/main/java/eu/iums/androidhka/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSendMessage = this.findViewById(R.id.btnSendMessage);
+
+        EditText edtMessage = this.findViewById(R.id.edtMessage);
+        TextView txtMessage = this.findViewById(R.id.txtMessage);
+
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = edtMessage.getText().toString();
+                txtMessage.setText(message);
+            }
+        });
     }
 
     @Override
